9551770ec437419896bc413434cd48e80048c278
Keisuke Kobayashi <kobakei122@gmail.com>
2017-11-09 17:31:43 +0900

    Initial commit

---
+++ app/src/main/java/io/github/kobakei/androidnotificationshowcase/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.kobakei.androidnotificationshowcase
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_lauyout)
+    }
+}
